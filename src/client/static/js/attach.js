91cf0e7e039f586234d7667375dd6fc9bfb346c8
Kingsley Victor <javaprodigy56@gmail.com>
2020-03-18 12:33:04 +0100

    include chart.js

--- src/client/static/js/attach.js
+++ src/client/static/js/attach.js
@@ -249,6 +249,56 @@
   }; 
 
   const attachMainHandler = () => {
+    socket.connect();
+    const barchart = document.getElementById("bar_chart");
+    const piechart = document.getElementById("pie_chart");
+    const barctx = new Chart(barchart, {
+      type: 'bar',
+      data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+          label: '# of Votes',
+          data: [12, 19, 3, 5, 2, 3],
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+          ],
+          borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+                    beginAtZero: true
+                  }
+            }]
+          },
+          responsive: true
+      }
+  });
+    const piectx = new Chart(piechart, {
+      type: "pie",
+      data: {
+        labels: ["Jake", "Jenson", "Jeremy", "Jacob"],
+        datasets: [{
+          data: [1, 2, 3, 4],
+          backgroundColor: ["red", "yellow", "green", "blue"] 
+        }]
+      }
+    });
     const startSocket = () => {
       socket.emit("COUNT_VOTES_BY_VOTER", {
         token: localStorage.getItem("token")
@@ -256,8 +306,8 @@
     };
     const listenForEvents = () => {
       socket.on("COUNTED_FOR_VOTER", (data) => {
-        const textItem = document.getElementById("count_of_votes");
-        textItem.innerText = `${data.count}`;
+        // const textItem = document.getElementById("count_of_votes");
+        // textItem.innerText = `${data.count}`;
       });
     };
     const startAtIntervals = () => {
